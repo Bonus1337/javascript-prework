69ab73e36f914018471e15f1d4391f3dee10572b
Bonus1337 <10569@student.puzim.edu.pl>
2022-11-16 23:57:58 +0100

    Finish project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput, buttonRock, buttonPaper, buttonScissors;
 
 
 function getMoveName(argMoveId) {
@@ -38,12 +38,24 @@ function displayResult(argPlayerMove, argComputerMove) {
     }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(argButtonName + ' został kliknięty');
+  playerMove = argButtonName;
+  console.log('ruch gracza to: ' + playerMove);
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log('wylosowana liczba to: ' + randomNumber);
+  computerMove = getMoveName(randomNumber);
+  console.log('ruch komputera to: ' + computerMove);
+  displayResult(playerMove, computerMove);
+}
+buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function(){buttonClicked('kamień');});
+buttonPaper = document.getElementById('button-paper');
+buttonPaper.addEventListener('click', function(){buttonClicked('papier');});
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function(){buttonClicked('nożyce');});
+
+
+
+
